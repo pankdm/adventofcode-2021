028a6acd85c1be5a59cef48ad7ce232eb9aefda0
Dmitry Panin <pankdm@gmail.com>
2021-11-30 19:28:52 -0800

    [All] Setup AOC project similar to 2020 year

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+// Disable some unhelpful warnings
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_mut)]
+
+// Some basic includes to alwawys include
+use std::collections::{HashMap, HashSet, VecDeque};
+use std::env;
+use std::fs;
+
+extern crate aoc;
+use aoc::*;
+use days::*;
+
+pub fn read_main_input() -> Vec<String> {
+    let input = fs::read_to_string("input/day25/in.txt").unwrap();
+    // let input = fs::read_to_string("input/day07/demo.txt").unwrap();
+    to_lines(&input)
+}
+
+pub fn read_input_from_args(args: &Vec<String>) -> Vec<String> {
+    println!("args: {:?}", args);
+    if args.len() <= 1 {
+        return read_main_input();
+    }
+    let input = fs::read_to_string(&args[1]).unwrap();
+    to_lines(&input)
+}
+
+fn main() {
+    day01::main();
+    // let args: Vec<String> = env::args().collect();
+    // let lines = read_input_from_args(&args);
+
+    // dbg!(day15::part1(&lines));
+    // dbg!(day15::part2(&lines));
+}
