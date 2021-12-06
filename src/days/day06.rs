89d88e14fa83157b0bd12690af57374f00d1e0d8
Dmitry Panin <pankdm@gmail.com>
2021-12-05 21:32:15 -0800

    [Day 6] Refactor for simplicity

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -10,43 +10,34 @@ use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::*;
 
-pub fn part1(lines: &Vec<String>) -> i64 {
+pub fn solve(lines: &Vec<String>, days: usize) -> i64 {
     let mut nums = parse_ints(&lines[0], ",");
-    println!("len = {}", nums.len());
-    for day in 0..80 {
-        let mut next = nums.clone();
-        for i in 0..nums.len() {
-            if nums[i] == 0 {
-                next[i] = 6;
-                next.push(8);
-            } else {
-                next[i] -= 1;
-            }
-        }
-        nums = next;
-    }
-    nums.len() as i64
-}
-
-pub fn part2(lines: &Vec<String>) -> i64 {
-    let mut nums = parse_ints(&lines[0], ",");
-    let mut counts = HashMap::new();
+    let max = (*nums.iter().max().unwrap()).max(9);
+    let mut counts = vec![0; max as usize];
     for value in nums.iter() {
-        *counts.entry(*value).or_insert(0) += 1;  
+        counts[*value as usize] += 1;
     }
-    for day in 0..256 {
-        let mut next = HashMap::new();
-        for (k, v) in counts.iter() {
-            if *k == 0 {
-                *next.entry(8).or_insert(0) += v;
-                *next.entry(6).or_insert(0) += v;
+    for day in 0..days {
+        let mut next = vec![0; max as usize];
+        for (index, value) in counts.iter().enumerate() {
+            if index == 0 {
+                next[8] += value;
+                next[6] += value;
             } else {
-                *next.entry(*k - 1).or_insert(0) += v;
+                next[index - 1] += value;
             }
         }
         counts = next.clone();
     }
-    counts.values().sum()
+    counts.iter().sum()
+}
+
+pub fn part1(lines: &Vec<String>) -> i64 {
+    solve(lines, 80)
+}
+
+pub fn part2(lines: &Vec<String>) -> i64 {
+    solve(lines, 256)
 }
 
 pub fn read_main_input() -> Vec<String> {
@@ -61,13 +52,13 @@ mod tests {
     #[test]
     fn test_part1() {
         let lines = read_main_input();
-        assert_eq!(part1(&lines), -1);
+        assert_eq!(part1(&lines), 380758);
     }
 
     #[test]
     fn test_part2() {
         let lines = read_main_input();
-        assert_eq!(part2(&lines), -1);
+        assert_eq!(part2(&lines), 1710623015163);
     }
 }
 
